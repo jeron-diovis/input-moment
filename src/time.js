6110c8a197a77165b8909ea912ca4f472e02fed4
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2016-04-28 11:11:11 +0300

    Add min/max options for Time component

--- src/time.js
+++ src/time.js
@@ -1,9 +1,26 @@
 var cx = require('classnames');
 var React = require('react');
+var moment = require('moment');
 var InputSlider = require('react-input-slider');
+
+var fitRange = ([ min, max ], x) => Math.max(Math.min(x, max), min);
 
 module.exports = React.createClass({
   displayName: 'Time',
+
+  propTypes: {
+    moment: React.PropTypes.object.isRequired,
+    max: React.PropTypes.object.isRequired,
+    min: React.PropTypes.object.isRequired
+  },
+
+  getDefaultProps() {
+    return {
+      moment: moment(),
+      max: moment("23:59", "HH:mm"),
+      min: moment("00:00", "HH:mm")
+    };
+  },
 
   render() {
     var m = this.props.moment;
@@ -20,16 +37,17 @@ module.exports = React.createClass({
           <div className="time-text">Hours:</div>
           <InputSlider
             className="u-slider-time"
-            xmin={0}
-            xmax={23}
+            xmin={this.getMinHour()}
+            xmax={this.getMaxHour()}
             x={m.hour()}
             onChange={this.changeHours}
           />
+
           <div className="time-text">Minutes:</div>
           <InputSlider
             className="u-slider-time"
-            xmin={0}
-            xmax={59}
+            xmin={this.getMinMinute()}
+            xmax={this.getMaxMinute()}
             x={m.minute()}
             onChange={this.changeMinutes}
           />
@@ -41,12 +59,46 @@ module.exports = React.createClass({
   changeHours(pos) {
     var m = this.props.moment;
     m.hours(parseInt(pos.x, 10));
+    this.fit(m);
     this.props.onChange(m);
   },
 
   changeMinutes(pos) {
     var m = this.props.moment;
     m.minutes(parseInt(pos.x, 10));
+    this.fit(m);
     this.props.onChange(m);
+  },
+
+  getMaxHour() {
+    return this.props.max.hour();
+  },
+
+  getMinHour() {
+    return this.props.min.hour();
+  },
+
+  getMaxMinute() {
+    var { moment, max } = this.props;
+    return moment.hour() === max.hour() ? max.minute() : 59;
+  },
+
+  getMinMinute() {
+    var { moment, min } = this.props;
+    return moment.hour() === min.hour() ? min.minute() : 0;
+  },
+
+  fit(m) {
+    m.hours(this.fitHours(m.hours()));
+    m.minutes(this.fitMinutes(m.minutes()));
+    return m;
+  },
+
+  fitHours(value) {
+    return fitRange([ this.getMinHour(), this.getMaxHour() ], value);
+  },
+
+  fitMinutes(value) {
+    return fitRange([ this.getMinMinute(), this.getMaxMinute() ], value);
   }
 });
